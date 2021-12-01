0f11612f48ceb7a5d5dc2387056a69a7ee12be68
Alex Shvarev 2 <alex@shvarev.com>
2021-12-01 20:29:39 +0300

    v0.1.3 fixed sockets

--- AlexChatBackend/asgi.py
+++ AlexChatBackend/asgi.py
@@ -9,16 +9,20 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from apps import ws_routes
+import apps.ws_routes
+from apps.base.middleware import TokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AlexChatBackend.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    "websocket": URLRouter(
-        ws_routes.websocket_urlpatterns
-    )
+  'http': get_asgi_application(),
+  'websocket': TokenAuthMiddleware(
+        URLRouter(
+            apps.ws_routes.websocket_urlpatterns
+        )
+    ),
 })
--- apps/groups/consumers.py
+++ apps/groups/consumers.py
@@ -1,5 +1,4 @@
 import json
-
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
