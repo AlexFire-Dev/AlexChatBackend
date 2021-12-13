719aa4db6bfe803559e33f32a507ce814d4c5e17
alex <alex@shvarev.com>
2021-12-13 16:45:50 +0300

    v0.3.3 bug fixes

--- apps/groups/consumers.py
+++ apps/groups/consumers.py
@@ -76,7 +76,7 @@ class GroupConsumer(WebsocketConsumer):
                 }
             )
 
-            query = GroupMember.objects.filter(group=self.group).values('user')
+            query = GroupMember.objects.filter(group=self.group)
             query = NotificationToken.objects.filter(user__groups__group_members=query)
 
             for obj in query:
