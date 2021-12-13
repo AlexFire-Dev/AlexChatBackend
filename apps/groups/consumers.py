6fbc176bc887e90cec26f1df41b59675173c6e90
alex <alex@shvarev.com>
2021-12-13 14:14:43 +0300

    v0.3.2 bug fixes

--- apps/groups/consumers.py
+++ apps/groups/consumers.py
@@ -74,7 +74,7 @@ class GroupConsumer(WebsocketConsumer):
                 }
             )
 
-            query = NotificationToken.objects.filter(user__groups__group_members=self.member)
+            query = NotificationToken.objects.filter(user__groups__group_members=self.group)
 
             for obj in query:
                 if message.author.user != obj.user:
