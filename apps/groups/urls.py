03b7ae190f8d7c7a070a675366649fecb1a21ce2
alex <alex@shvarev.com>
2021-12-14 16:32:35 +0300

    v0.6.3 bug fixes

--- apps/groups/urls.py
+++ apps/groups/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', GroupViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('<int:pk>/', GroupViewSet.as_view({'get': 'retrieve', 'put': 'partial_update', 'delete': 'destroy'})),
     path('<int:pk>/messages/', GroupMessageViewSet.as_view({'get': 'list'})),
-    path('<int:pk>/invitelink/', GroupMessageViewSet.as_view({'get': 'retrieve'})),
+    path('<int:pk>/invitelink/', GroupInviteLinkViewSet.as_view({'get': 'retrieve'})),
 
     path('membership/', GroupMemberViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('membership/<int:pk>/', GroupMemberViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'})),
